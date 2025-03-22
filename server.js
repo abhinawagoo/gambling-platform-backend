dace70e05292de1129013da1c8c31d07e5b8e076
abhinawagoo <abhinawago@gmail.com>
2025-03-23 02:24:53 +0530

    updating

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(
   cors({
     origin:
       process.env.FRONTEND_URL ||
-      "https://gambling-platform-frontend.vercel.app/",
+      "https://gambling-platform-frontend.vercel.app",
     credentials: true,
   })
 );
